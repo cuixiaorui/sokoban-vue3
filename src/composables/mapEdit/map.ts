7efe329f76beb4bd3fb85bd3ed10d6897f87a941
cuixiaorui <cui_xiaorui@126.com>
2023-10-01 11:07:23 +0800

    fix： update row/col value

--- src/composables/mapEdit/map.ts
+++ src/composables/mapEdit/map.ts
@@ -1,4 +1,4 @@
-import { reactive, watch } from "vue";
+import { reactive } from "vue";
 import { TileType } from "./tile";
 import { generateId } from "../../utils/id";
 
@@ -26,68 +26,67 @@ function createMapBlock(x: number, y: number, type?: MapType): MapBlock {
 }
 
 const map = reactive<Map>({
-  row: 8,
-  col: 8,
+  row: 0,
+  col: 0,
   data: [],
 });
 
 export function useMap() {
-  // 处理列
-  watch(
-    () => map.col,
-    (newCol, oldCol) => {
-      const addColCount = newCol - oldCol;
+  function updateMapCol(newCol: number) {
+    const oldCol = map.col;
+    const addColCount = newCol - oldCol;
 
-      // 添加列
-      if (addColCount > 0) {
-        let col = map.data[0].length;
+    // 添加列
+    if (addColCount > 0) {
+      let col = map.data[0].length;
 
-        for (let i = 0; i < map.data.length; i++) {
-          for (let j = 0; j < addColCount; j++) {
-            const block = createMapBlock(col + j, i);
-            map.data[i].push(block);
-          }
+      for (let i = 0; i < map.data.length; i++) {
+        for (let j = 0; j < addColCount; j++) {
+          const block = createMapBlock(col + j, i);
+          map.data[i].push(block);
         }
-      } else if (addColCount < 0) {
-        // 删除
-        for (let i = 0; i < map.data.length; i++) {
-          for (let j = 0; j < Math.abs(addColCount); j++) {
-            map.data[i].pop();
-          }
+      }
+    } else if (addColCount < 0) {
+      // 删除
+      for (let i = 0; i < map.data.length; i++) {
+        for (let j = 0; j < Math.abs(addColCount); j++) {
+          map.data[i].pop();
         }
       }
     }
-  );
 
-  watch(
-    () => map.row,
-    (newRow, oldRow) => {
-      const addRowCount = newRow - oldRow;
+    map.col = newCol;
+  }
 
-      // 添加行
-      if (addRowCount > 0) {
-        for (let i = 0; i < addRowCount; i++) {
-          // 先创建一行
-          const newRow: any = [];
-          map.data.push(newRow);
+  function updateMapRow(newRow: number) {
+    const oldRow = map.row;
+    const addRowCount = newRow - oldRow;
 
-          // 在把新的行填满
-          let col = map.data[i].length;
+    // 添加行
+    if (addRowCount > 0) {
+      for (let i = 0; i < addRowCount; i++) {
+        // 先创建一行
+        const newRow: any = [];
+        map.data.push(newRow);
 
-          for (let j = 0; j < col; j++) {
-            const block = createMapBlock(j, map.data.length - 1);
-            newRow.push(block);
-          }
-        }
-      } else if (addRowCount < 0) {
-        // 删除
-        for (let i = 0; i < Math.abs(addRowCount); i++) {
-          // 删除最后一行
-          map.data.pop();
+        // 在把新的行填满
+        let col = map.data[i].length;
+
+        for (let j = 0; j < col; j++) {
+          const block = createMapBlock(j, map.data.length - 1);
+          newRow.push(block);
         }
       }
+    } else if (addRowCount < 0) {
+      // 删除
+      for (let i = 0; i < Math.abs(addRowCount); i++) {
+        // 删除最后一行
+        map.data.pop();
+      }
     }
-  );
+
+    map.row = newRow;
+  }
 
   function initMap() {
     for (let i = 0; i < map.row; i++) {
@@ -102,5 +101,7 @@ export function useMap() {
   return {
     map,
     initMap,
+    updateMapCol,
+    updateMapRow,
   };
 }
--- src/view/MapEdit.vue
+++ src/view/MapEdit.vue
@@ -19,11 +19,11 @@
       <div>
         <div>
           row:
-          <input type="text" v-model.number="map.row" />
+          <input type="text" v-model.number="row" />
         </div>
         <div>
           col:
-          <input type="text" v-model.number="map.col" />
+          <input type="text" v-model.number="col" />
         </div>
       </div>
       <EditElementView></EditElementView>
@@ -36,12 +36,27 @@ import MapDataDisplay from "../components/mapEdit/MapDataDisplay.vue";
 import EditElementView from "../components/mapEdit/EditElementView.vue";
 import Cargos from "../components/mapEdit/Cargos.vue";
 import PlacePoints from "../components/mapEdit/PlacePoints.vue";
-import Keeper from "../components/mapEdit/Keeper.vue"; import MapBlock from "../components/mapEdit/MapBlock.vue";
+import Keeper from "../components/mapEdit/Keeper.vue";
+import MapBlock from "../components/mapEdit/MapBlock.vue";
 import { useMap } from "../composables/mapEdit/map";
+import { ref, watchEffect } from "vue";
 
-const { map, initMap } = useMap();
+const { map, initMap, updateMapRow, updateMapCol } = useMap();
 
-initMap()
+const row = ref(8);
+const col = ref(8);
+
+watchEffect(() => {
+  if (!row.value) return;
+  updateMapRow(row.value)
+});
+
+watchEffect(() => {
+  if(!col.value) return;
+  updateMapCol(col.value)
+})
+
+initMap();
 </script>
 
 <style scoped></style>
